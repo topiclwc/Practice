d563c3ec51f8b677d0d2626f96f0d6bd7ad1ad5b
topiclwc@gmail.com <topiclwc@gmail.com>
2019-06-09 12:08:12 -0700

    Passing the values via attribute

--- TOPICLWC-TEST/src/aura/TestComponent/TestComponentController.js
+++ TOPICLWC-TEST/src/aura/TestComponent/TestComponentController.js
@@ -1,9 +1,8 @@
 // Controller
 ({
-	handleCalculate : function(component, event, helper) {
-		var fnum = parseInt(component.find("fnum_id").get("v.value"));
-       	var snum = parseInt(component.find("snum_id").get("v.value"));
-        var result = fnum + snum;
-        component.find("rnum_id").set("v.value",result);
+	doCalculate : function(component, event, helper) {
+
+		helper.handleCalculate(component, event);
+		
 	}
 })
--- TOPICLWC-TEST/src/aura/TestComponent/TestComponentHelper.js
+++ TOPICLWC-TEST/src/aura/TestComponent/TestComponentHelper.js
@@ -1,5 +1,9 @@
+//helper
 ({
-	helperMethod : function() {
-		
+	handleCalculate : function(component, event) {
+		var fnumber = parseInt(component.get("v.fNumber"));
+		var lnumber = parseInt(component.get("v.lNumber"));
+		var result = fnumber + lnumber;
+		component.set("v.result", result);
 	}
 })
