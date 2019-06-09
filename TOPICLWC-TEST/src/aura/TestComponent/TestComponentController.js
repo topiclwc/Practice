48653b1d6f3644a71183500dd9158c799d18aa6c
topiclwc@gmail.com <topiclwc@gmail.com>
2019-06-09 11:15:28 -0700

    initial Commit for

---
+++ TOPICLWC-TEST/src/aura/TestComponent/TestComponentController.js
@@ -0,0 +1,8 @@
+({
+	handleCalculate : function(component, event, helper) {
+		var fnum = parseInt(component.find("fnum_id").get("v.value"));
+       	var snum = parseInt(component.find("snum_id").get("v.value"));
+        var result = fnum + snum;
+        component.find("rnum_id").set("v.value",result);
+	}
+})
