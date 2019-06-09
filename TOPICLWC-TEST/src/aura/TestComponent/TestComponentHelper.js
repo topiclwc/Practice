cf90a61d513059bd67d511ba40fa10987c0441cc
topiclwc@gmail.com <topiclwc@gmail.com>
2019-06-09 13:46:02 -0700

    second way

--- TOPICLWC-TEST/src/aura/TestComponent/TestComponentHelper.js
+++ TOPICLWC-TEST/src/aura/TestComponent/TestComponentHelper.js
@@ -3,7 +3,19 @@
 	handleCalculate : function(component, event) {
 		var fnumber = parseInt(component.get("v.fNumber"));
 		var lnumber = parseInt(component.get("v.lNumber"));
-		var result = fnumber + lnumber;
-		component.set("v.result", result);
+		var calculateResult = component.get("c.getCalculateResult");
+		calculateResult.setParams({"fnum":fnumber, "lnum": lnumber});
+		
+		//setCallback
+		calculateResult.setCallback(this, function(response){
+			var addResult = response.getReturnValue();
+			//alert(addResult);
+            component.set("v.result",addResult);
+		});
+		
+		//enqueueAction 
+		$A.enqueueAction(calculateResult);
+ 
+
 	}
 })
